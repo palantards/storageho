f1fbaea122a4afbde139fa5267d346bb0b2423b0
Gurk1992 <Gurk1992@users.noreply.github.com>
2026-03-10 15:52:48 +0100

    added vercel speed test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { defaultLocale } from "@/i18n/config";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootPage() {
   redirect(`/${defaultLocale}`);
 }
-
